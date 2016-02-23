35476e78a6a39caee87f20ae5d79a43fe0f9b62a
Dean Ventutec <dquinney@ventutec.com>
2016-02-23 17:09:18 +0000

    got counters going up

--- app/pages/counter/counter.js
+++ app/pages/counter/counter.js
@@ -6,9 +6,23 @@ import {Page} from 'ionic-framework/ionic';
 export class CounterPage {
     constructor() {
 
+        this.counters = [
+            {
+                name: 'Test 1',
+                count: 1
+            },
+            {
+                name: 'Test 2',
+                count: 2
+            }
+        ]
+
+        console.log(this.counters);
+
     }
 
-    countIncrement(){
-        console.log(this);
+    countIncrement(counter) {
+        //console.log(this);
+        counter.count++;
     }
 }
