313a50d6d7847b8e5e96e11064dc0790fce26782
Dean Ventutec <dquinney@ventutec.com>
2016-02-23 13:49:22 +0000

    remove other pages

--- app/pages/counter/counter.js
+++ app/pages/counter/counter.js
@@ -1,11 +1,11 @@
-import {Page, NavController} from 'ionic-framework/ionic';
+import {Page} from 'ionic-framework/ionic';
 
 @Page({
     templateUrl: 'build/pages/counter/counter.html'
 })
 export class CounterPage {
-    constructor(nav:NavController) {
-        this.nav = nav;
+    constructor() {
+
     }
 
     countIncrement(){
