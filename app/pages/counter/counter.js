f9f40d688efee3674efb57157a4e93be6d254949
Dean Ventutec <dquinney@ventutec.com>
2016-02-25 16:55:05 +0000

    got time since working, still need to wrap in if statement

--- app/pages/counter/counter.js
+++ app/pages/counter/counter.js
@@ -1,4 +1,5 @@
 import {Page} from 'ionic-framework/ionic';
+import * as dateFns from 'date-fns';
 
 @Page({
     templateUrl: 'build/pages/counter/counter.html'
@@ -9,20 +10,37 @@ export class CounterPage {
         this.counters = [
             {
                 name: 'Test 1',
-                count: 1
+                count: 1,
+                lastUpdated: null,
+                sinceUpdated: null
             },
             {
                 name: 'Test 2',
-                count: 2
+                count: 2,
+                lastUpdated: null,
+                sinceUpdated: null
             }
-        ]
+        ];
 
-        console.log(this.counters);
+        console.log(this.counters[0]);
+
+        setInterval(()=>{
+            for (let counter of this.counters){
+                if(counter.lastUpdated != null){
+                    counter.sinceUpdated = dateFns.differenceInSeconds(Date.now(), counter.lastUpdated);
+                }
+            }
+        }, 1000)
 
     }
 
     countIncrement(counter) {
-        //console.log(this);
+        console.clear();
         counter.count++;
+
+        console.log(dateFns.differenceInSeconds(Date.now(), counter.lastUpdated));
+
+        counter.lastUpdated = Date.now(); //timestamp in seconds
+        console.log(this.counters[0]);
     }
 }
