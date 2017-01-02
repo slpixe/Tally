34ae08dd4a60d1aaafa24d93b283d9e1ed68c142
Slpixe <slpixe@gmail.com>
2017-01-02 05:12:10 +0000

    counters save/overwrite ok, just need to finish off providers methods for getting/deleting etc

--- src/providers/counters.ts
+++ src/providers/counters.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { Storage } from '@ionic/storage';
-import { Uuid } from 'uuid/v4';
 import { Counter } from '../models/counter';
 
 @Injectable()
@@ -8,60 +7,32 @@ export class Counters {
 
   private COUNTERS_KEY: string = '_counters';
 
-  counters: any;
+  counters: any = [];
 
   _readyPromise: Promise<any>;
 
   constructor(public storage: Storage) {
+    this.load();
   }
 
-  //load() {
-  //  return this.storage.get(this.COUNTERS_KEY).then((value) => {
-  //    if(value) {
-  //      this.counters = value;
-  //    }
-  //  });
-  //}
-
-  //merge(counters: any) {
-  //  for(let k in counters) {
-  //    this.counters[k] = counters[k];
-  //  }
-  //  return this.save();
-  //}
+  load() {
+    return this.storage.get(this.COUNTERS_KEY).then((value) => {
+      if(value) {
+        this.counters = value;
+      }
+    });
+  }
 
   save(counter: Counter) {
-    console.log(counter);
 
     if(counter.getUuid() == null) {
       console.log('no uuid');
-      let newUuid = Uuid();
-      console.log(newUuid);
+      let newUuid = Date.now();
       counter.setUuid(newUuid);
     }
 
-    //this.counters[key] = value;
-    //console.log(this.counters);
-    //return this.storage.set(this.COUNTERS_KEY, this.counters);
-    //return this.storage.set(this.COUNTERS_KEY, value);
-    //this.storage.set('name', 'Mr.Iron');
-  }
-
-  setAll(value: any) {
-    //return this.storage.set(this.COUNTERS_KEY, value);
-  }
-
-  getValue(key: string) {
-    //return this.storage.get(key);
-  }
-
-  store() {
-    return this.setAll(this.counters);
-  }
-
-  get allcounters() {
-    return 'hello';
-    //return this.counters;
+    this.counters[counter.getUuid()] = counter;
+    return this.storage.set(this.COUNTERS_KEY, this.counters);
   }
 
 }
